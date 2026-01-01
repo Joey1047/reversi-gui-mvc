ecbe4f3ebc2255189836951febb2b8a8624f4177
Joey1047 <zhaoyil@arizona.edu>
2026-01-01 15:32:43 -0700

    Update Reversi.java

--- src/Reversi.java
+++ src/Reversi.java
@@ -1,1 +1,26 @@
+package view;
 
+import javafx.application.Application;
+
+/**
+ * The main entry point for the CSC 335 Project 5 Reversi program.
+ * <p>
+ * This class launches the JavaFX application by invoking
+ * {@link ReversiView}, which constructs the GUI and connects
+ * the MVC components (Model – View – Controller).
+ * </p>
+ *
+ * @author Your Name
+ * @version Fall 2025
+ */
+public class Reversi {
+
+    /**
+     * Launches the Reversi graphical user interface.
+     *
+     * @param args command-line arguments (unused)
+     */
+    public static void main(String[] args) {
+        Application.launch(ReversiView.class, args);
+    }
+}
